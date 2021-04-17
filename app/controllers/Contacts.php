e564514005f9e747cf0b53c579d09d0a50bf5901
DanielMikolajczyk <56119744+DanielMikolajczyk@users.noreply.github.com>
2021-04-17 10:24:52 +0200

    contact form

--- app/controllers/Contacts.php
+++ app/controllers/Contacts.php
@@ -1,9 +1,86 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+
 class Contacts extends Controller
 {
   public function index()
   {
     return $this->view('/contacts/index');
+  }
+
+  public function store()
+  {
+    if(!($_SERVER['REQUEST_METHOD'] == 'POST'))
+    {
+      //TODO 404 error
+      header("HTTP/1.0 404 Not Found");
+      exit();
+    }
+    $_POST = filter_input_array(INPUT_POST,FILTER_SANITIZE_STRING);
+    $data = [
+      'name' => trim($_POST['name']),
+      'email' => trim($_POST['email']),
+      'title' => trim($_POST['title']),
+      'body' => trim($_POST['body']),
+      'nameError' => '',
+      'emailError' => '',
+      'titileError' => '',
+      'bodyError' => '',
+    ];
+
+    if(!filter_var($data['email'], FILTER_VALIDATE_EMAIL))
+    {
+      $data['emailError'] = 'Invalid email adress!';
+    }
+    if(strlen($data['name'])>64)
+    {
+      $data['nameError'] = 'Name is too long!';
+    }
+    if(strlen($data['title'])>64)
+    {
+      $data['titleError'] = 'Title is too long!';
+    }
+    if(strlen($data['body'])>1024)
+    {
+      $data['bodyError'] = 'Too many characters!';
+    }
+    
+    $mail = new PhpMailer(true);
+    try {
+      //Server settings
+      $mail->SMTPDebug = 0;                      //Enable verbose debug output
+      $mail->isSMTP();                                            //Send using SMTP
+      $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+      $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+      $mail->Username   = 'dyskeeeit1@gmail.com';                     //SMTP username
+      $mail->Password   = 'piotrdziurdzia1@';                               //SMTP password
+      $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+      $mail->Port       = 587;
+      $mail->CharSet = 'UTF-8';
+
+      //Recipients
+      $mail->setFrom('dyskeeeit1@gmail.com', 'Kontakt NetBuild');
+      #$mail->addAddress('d.mikolajczyk@netbuild.pl', 'Damian');     //Add a recipient
+      $mail->addAddress('d.mikolajczyk.20@gmail.com', 'Daniel');     //Add a recipient
+  
+      //Content
+      $mail->isHTML(true);                                  //Set email format to HTML
+      $mail->Subject = 'Kontakt NetBuild - Zapytanie od klienta';
+      $mail->Body    = 'Otrzymałeś maila od: <b>'.$data['name'].'</b>'.
+                       '<br> Tytuł wiadmości: <b>'. $data['title']. '</b>'. 
+                       '<br> Treść: <b>'. $data['body'].'</b>'. 
+                       '<br> Mail kontaktowy: '. $data['email'];
+      $mail->AltBody = 'Twój email nie obsługuje wiadmości HTML.';
+
+      $mail->send();
+      header("Location: ".URLROOT."/contacts?success=1");
+      exit();
+    } catch (Exception $e) {
+      echo 'Error. '.$mail->ErrorInfo;
+    }    
   }
 }
--- app/views/contacts/index.php
+++ app/views/contacts/index.php
@@ -14,13 +14,43 @@
   </div>
   <div class="my-5">
     <div class="main-width mx-auto py-5">
-      <h4 class="text-primary-o">Skonaktuj się z nami</h4>
-      <form action="">
+      <h4 class="text-primary-o text-center">Skonaktuj się z nami</h4>
+      <?php if($_GET['success']): ?>
+        <h6 class="text-dgreen text-center">Wiadomość wysłana pomyślnie</h6>
+      <?php elseif($data['error']):?>
+        <h6 class="text-primary-o text-center">Nie udało się, wysłać wiadmości</h6>
+      <?php endif;?>
+      <form action="<?= URLROOT ?>/contacts/store" method="POST">
+          <div class="border text-center">
 
+            <div class="my-4">
+              <h4 class="my-2 text-primary-o">Nazwa:</h4>
+              <input autocomplete="off" type="text" name="name" class="input-o">
+            </div>
 
-      </form>
-    <script src="<?= URLROOT ?>/js/map.js"></script>
-    <div id="map"></div>
+            <div class="my-4">
+              <h4 class="my-2 text-primary-o">Email:</h4>
+              <input autocomplete="off" type="text" name="email" class="input-o">
+            </div>
+
+            <div class="my-4">
+              <h4 class="my-2 text-primary-o">Tytuł:</h4>
+              <input autocomplete="off" type="text" name="title" class="input-o">
+            </div>
+            
+            <div class="my-4">
+              <h4 class="my-2 text-primary-o">Treść:</h4>
+
+              <textarea name="body" class="text-area-o"></textarea>
+            </div>
+            <div class="my-3">
+              <button class="btn-primary-o" type="submit" name="submit">Wyślij wiadomość!</button>
+            </div>
+          </div>
+        </form>
+        <h4 class="text-primary-o text-center my-5">Nasza lokalizacja</h4>
+      <script src="<?= URLROOT ?>/js/map.js"></script>
+      <div id="map" class="my-3 mx-auto"></div>
     </div>
   </div>
 </main>
